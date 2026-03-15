eaf77123a61158d5c2d09b41ece13793f3ef8d16
dnfntk13 <dnfntk13@gmail.com>
2026-03-15 07:04:10 +0000

    minor fix

--- app/patient/dashboard.tsx
+++ app/patient/dashboard.tsx
@@ -450,7 +450,7 @@ export default function PatientDashboardScreen() {
                   style: "destructive",
                   onPress: async () => {
                     await store.clearCurrentUser();
-                    router.replace("/auth/role-select" as any);
+                    router.replace("/" as any);
                   },
                 },
               ],
